6fd7514adc5146bf933af7915097633870c4d301
Vittorio Passuello-Dussault <vittorio-passduss@outlook.com>
2018-12-23 15:37:18 -0500

    done basic modulo calculator

---
+++ app/src/main/java/vitonimal/modulocalculator/CalcActivity.java
@@ -0,0 +1,41 @@
+package vitonimal.modulocalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CalcActivity extends AppCompatActivity {
+
+    EditText operandAInput;
+    EditText operandBInput;
+    EditText resultDisplay;
+    Button moduloSubmit;
+
+    View.OnClickListener moduloListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int operandA = Integer.valueOf(operandAInput.getText().toString());
+            int operandB = Integer.valueOf(operandBInput.getText().toString());
+            int moduloResult = operandA % operandB;
+
+            resultDisplay = findViewById(R.id.resultDisplay);
+            resultDisplay.setText(String.valueOf(moduloResult));
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+
+        operandAInput = findViewById(R.id.operandAInput);
+        operandBInput = findViewById(R.id.operandBInput);
+        moduloSubmit = findViewById(R.id.moduloSubmit);
+
+        moduloSubmit.setOnClickListener(moduloListener);
+
+    }
+}
